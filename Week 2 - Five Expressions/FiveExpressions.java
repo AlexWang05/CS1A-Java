49cd4d83be89ff7ac8709da25a4b68c41eea407c
AlexWang05 <hpwzz20@gmail.com>
2020-12-10 08:18:10 -0800

    Create FiveExpressions.java

---
+++ Week 2 - Five Expressions/FiveExpressions.java
@@ -0,0 +1,82 @@
+/*
+ * Class: CS1A
+ * Description: Programming Assignment 2 Option A - Five Expressions:
+ *              creating 2 integer variables and computing new values bases on the two numbers.
+ *              Printing five values/expressions in output.
+ * Due date: 10/5/2020
+ * Name: Zhaozhong (Alex) Wang
+ * File name: FiveExpressions.java
+*/
+
+
+package fiveexpressions_week2;
+
+public class FiveExpressions {
+
+	public static void main(String[] args) {
+		
+		int myId = 20456413;   //storing my Foothill ID number in the myId variable
+		int numLet = 9;   //storing the number of letters in my first name (9)
+		
+		
+		System.out.println("My first name is Zhaozhong");   //printing required info
+		System.out.println("My student ID is " + myId);
+		System.out.println("The number of characters in my first name is " + numLet);
+		
+		
+		System.out.println();   //printing empty line for space
+		
+		
+		int intResult = myId % 17;   //calculating and storing value #1
+		System.out.println("Expression #1 --> " + intResult);
+		
+		
+		System.out.println();
+		
+		
+		intResult = (numLet + 17) % 11;   //calculating and storing value #2
+		System.out.println("Expression #2 --> " + intResult);
+		
+		
+		System.out.println();
+		
+		
+		double doubleResult = (double)myId / (numLet + 800);   //calculating and storing value #3
+		System.out.println("Expression #3 --> " + doubleResult);   
+		
+		
+		System.out.println();
+		
+		
+		intResult = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + numLet;   //calculating and storing value #4
+		System.out.println("Expression #4 --> " + intResult);   
+		
+		
+		System.out.println();
+		
+		
+		doubleResult = 15000 / (80 + ((myId - 123456) / ((numLet + 20) *
+				((double)numLet + 20))));   //calculating and storing value #5
+		System.out.println("Expression #5 --> " + doubleResult);
+		
+		
+		/*SAMPLE RUN
+		 * 
+		 * My first name is Zhaozhong
+		 * My student ID is 20456413
+		 * The number of characters in my first name is 9
+		 * 
+		 * Expression #1 --> 7
+		 * 
+		 * Expression #2 --> 4
+		 * 
+		 * Expression #3 --> 25286.04820766378
+		 * 
+		 * Expression #4 --> 45
+		 * 
+		 * Expression #5 --> 0.6183751688767145
+		 */
+		
+	}
+
+}

f5e283a3cd5cba00c45e5b03b2c11219111e44c4
AlexWang05 <hpwzz20@gmail.com>
2020-12-10 08:16:28 -0800

    Update Code Directory
    Changing folder names and uploading week 1 code.

---
+++ Week 1 - Web Sites/WebSites.java
@@ -0,0 +1,26 @@
+/* Class: F20 CS F001A OBJ-ORIENTED PROG METHOD JAVA 04W
+ * Instructor: Hanan Ibrahim
+ * Description: Printing 3 websites with names and URL
+ * Due: 9/2/2020
+ * I pledge by honor that I have completed the programming assignment independently.
+ * I have not copied the code from a student or any source
+ * I have not given my code to any student
+ * Sign Here: Zhaozhong Wang
+ */
+
+
+
+
+package websites_week1; 
+
+public class WebSites { 
+
+	public static void main(String[] args) {
+		//printing website name and URL using escape sequence
+		System.out.println("Google:\nhttps://google.com"); 
+		System.out.println("Youtube:\nhttps://youtube.com");
+		System.out.println("Gmail:\nhttps://gmail.com");
+
+	}
+
+}

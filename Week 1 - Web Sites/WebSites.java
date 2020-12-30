0d176cc1173e3feac1e5d5854d13fd84de1e11dd
AlexWang05 <73456512+AlexWang05@users.noreply.github.com>
2020-12-29 21:46:48 -0800

    Edit comments and fix readability issues

--- Week 1 - Web Sites/WebSites.java
+++ Week 1 - Web Sites/WebSites.java
@@ -1,26 +1,14 @@
-/* Class: F20 CS F001A OBJ-ORIENTED PROG METHOD JAVA 04W
- * Instructor: Hanan Ibrahim
- * Description: Printing 3 websites with names and URL
- * Due: 9/2/2020
- * I pledge by honor that I have completed the programming assignment independently.
- * I have not copied the code from a student or any source
- * I have not given my code to any student
- * Sign Here: Zhaozhong Wang
+/* 
+ * Printing 3 websites with names and URL
  */
-
-
-
 
 package websites_week1; 
 
 public class WebSites { 
-
 	public static void main(String[] args) {
 		//printing website name and URL using escape sequence
 		System.out.println("Google:\nhttps://google.com"); 
 		System.out.println("Youtube:\nhttps://youtube.com");
 		System.out.println("Gmail:\nhttps://gmail.com");
-
 	}
-
 }

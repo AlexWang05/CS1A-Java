46310c2d84e39a65ca3f7d5578f09788b1c52849
alexwang05 <hpwzz20@gmail.com>
2021-06-12 11:01:06 -0700

    Update documentation and sample run

--- Week 2 - Five Expressions/FiveExpressions.java
+++ Week 2 - Five Expressions/FiveExpressions.java
@@ -1,54 +1,59 @@
-/*
- * Assignment 2A - Five Expressions: creating 2 integer variables and computing new values bases on the two numbers.
- *              Printing five values/expressions in output.
-*/
-
-package fiveexpressions_week2;
+/**
+ * Assignment 2A
+ * Creates 2 integers and computes new values bases on the two integers.
+ * prints five values in output.
+ * 
+ * @author Alex Wang
+ */
 
 public class FiveExpressions {
 
 	public static void main(String[] args) {
-		
-		int myId = 20456413;   //storing my Foothill ID number in the myId variable
-		int numLet = 9;   //storing the number of letters in my first name (9)
-		
-		System.out.println("My first name is Zhaozhong");   //printing required info
+
+		int myId = 20456414; // stores ID number in myId
+		int numLet = 9; // stores the number of letters in my first name
+
+		// prints required info
+		System.out.println("My first name is Zhaozhong");
 		System.out.println("My student ID is " + myId);
 		System.out.println("The number of characters in my first name is " + numLet);
-		
-		int intResult = myId % 17;   //calculating and storing value #1
+
+		// calculating and storing value #1
+		int intResult = myId % 17;
 		System.out.println("\nExpression #1 --> " + intResult);
 
-		intResult = (numLet + 17) % 11;   //calculating and storing value #2
+		// value 2
+		intResult = (numLet + 17) % 11;
 		System.out.println("\nExpression #2 --> " + intResult);
 
-		double doubleResult = (double)myId / (numLet + 800);   //calculating and storing value #3
-		System.out.println("\nExpression #3 --> " + doubleResult);   
+		// value 3
+		double doubleResult = (double) myId / (numLet + 800); 
+		System.out.println("\nExpression #3 --> " + doubleResult);
 		
-		intResult = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + numLet;   //calculating and storing value #4
-		System.out.println("\nExpression #4 --> " + intResult);   
+		// value 4
+		intResult = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + numLet; 
+		System.out.println("\nExpression #4 --> " + intResult);
 
-		doubleResult = 15000 / (80 + ((myId - 123456) / ((numLet + 20) *
-				((double)numLet + 20))));   //calculating and storing value #5
+		// value 5
+		doubleResult = 15000 / (80 + ((myId - 123456) / ((numLet + 20) * ((double) numLet + 20)))); 
 		System.out.println("\nExpression #5 --> " + doubleResult);
-		
-		
-		/*SAMPLE RUN
+
+		/*
+		 * SAMPLE RUN
 		 * 
-		 * My first name is Zhaozhong
+		 * My first name is Zhaozhong 
 		 * My student ID is 20456413
 		 * The number of characters in my first name is 9
 		 * 
-		 * Expression #1 --> 7
+		 * Expression #1 --> 8
 		 * 
 		 * Expression #2 --> 4
 		 * 
-		 * Expression #3 --> 25286.04820766378
+		 * Expression #3 --> 25286.049443757725
 		 * 
 		 * Expression #4 --> 45
 		 * 
-		 * Expression #5 --> 0.6183751688767145
+		 * Expression #5 --> 0.61837513856456
 		 */
-		
 	}
 }

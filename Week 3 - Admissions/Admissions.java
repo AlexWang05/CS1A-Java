b6f17c894d4dce9bd518bfb6743aba4a5ed52ee1
alexwang05 <hpwzz20@gmail.com>
2021-06-12 11:22:31 -0700

    remove redundant comments

--- Week 3 - Admissions/Admissions.java
+++ Week 3 - Admissions/Admissions.java
@@ -1,134 +1,138 @@
-/*
-* Assignment 3B: This program uses the SAT/ACT scores and GPA to determine
-*              who seems to be the stronger applicant.
-*/
-
-package admissions_week3;
+/**
+ * Assignment 3B
+ * Uses SAT/ACT scores and GPA to determine a stronger applicant.
+ * 
+ * @author Alex Wang
+ */
 
 import java.util.Scanner;
 
 public class Admissions {
 	public static void main(String[] args) {
-		System.out.println("This program compares two applicants to\n"
-				+ "determine which one seems like the stronger\n"
-				+ "applicant. For each candidate I will need\n"
-				+ "either SAT or ACT scores plus a weighted GPA.\n"); //printing required info
-		
-		Scanner input = new Scanner(System.in); //creating Scanner object
-		
-		System.out.println("Information for the first applicant:"
-				+ "\n\tdo you have 1) SAT scores or 2) ACT scores? ");
-		int scoreChoice = input.nextInt(); //assigning choice to variable scoreChoice
-		
-		double satScore = 0; //initializing satScore
-		double actScore = 0; //initializing actScore
-		
-		if (scoreChoice == 1) { //if SAT scores are chosen
+		// printing required info
+		System.out.println("This program compares two applicants to\n" + "determine which one seems like the stronger\n"
+				+ "applicant. For each candidate I will need\n" + "either SAT or ACT scores plus a weighted GPA.\n");
+
+		Scanner input = new Scanner(System.in); // Scanner object
+
+		System.out.println("Information for the first applicant:" + "\n\tdo you have 1) SAT scores or 2) ACT scores? ");
+		int scoreChoice = input.nextInt(); // assigns choice to variable scoreChoice
+
+		// initialize scores
+		double satScore = 0;
+		double actScore = 0;
+
+		// if SAT scores are chosen
+		if (scoreChoice == 1) {
 			System.out.println("SAT math? ");
-			int satMath = input.nextInt(); //assigning math score
-			
+			int satMath = input.nextInt(); // assigns math score
+
 			System.out.println("SAT verbal? ");
-			int satVerbal = input.nextInt(); //assigning verbal score
-			
-			satScore = (double)(2 * satVerbal + satMath )/24; //calculate SAT score
+			int satVerbal = input.nextInt(); // assigns verbal score
+
+			satScore = (double)((2 * satVerbal + satMath) / 24); // calculate SAT score
 		}
-		
-		else if (scoreChoice == 2) { // if ACT scores are chosen
+
+		// if ACT scores are chosen
+		else if (scoreChoice == 2) {
 			System.out.println("ACT English? ");
-			int actEnglish = input.nextInt(); //assigning English score
-			
+			int actEnglish = input.nextInt(); // assigns English score
+
 			System.out.println("ACT math? ");
-			int actMath = input.nextInt(); //assigning math score
-			
+			int actMath = input.nextInt(); // assigns math score
+
 			System.out.println("ACT reading? ");
-			int actReading = input.nextInt(); //assigning reading score
-			
+			int actReading = input.nextInt(); // assigns reading score
+
 			System.out.println("ACT science? ");
-			int actScience = input.nextInt(); //assigning science score
-			
-			actScore = (double)((2 * actReading + actEnglish + actMath + actScience) / 1.8);
-			//calculating ACT score
+			int actScience = input.nextInt(); // assigns science score
+
+			actScore = (double)((2 * actReading + actEnglish + actMath + actScience) / 1.8); // calculate ACT score
 		}
-		
+
+		// GPA prompts
 		System.out.println("overall GPA? ");
-		double actualGPA = input.nextDouble(); //storing overall GPA
-		
+		double actualGPA = input.nextDouble();
+
 		System.out.println("Max GPA? ");
-		double maxGPA = input.nextDouble(); //storing max GPA
-		
-		double gpaScore = (actualGPA / maxGPA) * 100; //calculating total GPA Score
-		
-		double overallScore1 = 0; //initializing overall score variable for student 1
-		
-		if (scoreChoice == 1) //checking if SAT score was chosen/calculated
+		double maxGPA = input.nextDouble();
+
+		// calculate total GPA score
+		double gpaScore = (actualGPA / maxGPA) * 100; 
+
+		double overallScore1 = 0; // initialize student 1 overall score 
+
+		// calculate overall score
+		if (scoreChoice == 1)
 			overallScore1 = satScore + gpaScore;
-		
-		else if (scoreChoice == 2) //checking if ACT score was chosen/calculated
+
+		else if (scoreChoice == 2)
 			overallScore1 = actScore + gpaScore;
-		
-		//getting information for second applicant
-		System.out.println("\nInformation for the second applicant:"
-				+ "\n\tdo you have 1) SAT scores or 2) ACT scores? ");
-		scoreChoice = input.nextInt(); //assigning choice to variable scoreChoice
-		
-		if (scoreChoice == 1) { //if SAT scores are chosen
+
+		// second applicant information
+		System.out.println("\nInformation for the second applicant:" + "\n\tdo you have 1) SAT scores or 2) ACT scores? ");
+		scoreChoice = input.nextInt();
+
+		// SAT scores
+		if (scoreChoice == 1) { 
 			System.out.println("SAT math? ");
-			int satMath = input.nextInt(); //assigning math score
-			
+			int satMath = input.nextInt(); 
+
 			System.out.println("SAT verbal? ");
-			int satVerbal = input.nextInt(); //assigning verbal score
-			
-			satScore = (double)(2 * satVerbal + satMath )/24; //calculate SAT score
+			int satVerbal = input.nextInt(); 
+
+			satScore = (double)(2 * satVerbal + satMath) / 24; // calculate SAT score
 		}
-		
-		else if (scoreChoice == 2) { // if ACT scores are chosen
+
+		// ACT scores
+		else if (scoreChoice == 2) {
 			System.out.println("ACT English? ");
-			int actEnglish = input.nextInt(); //assigning English score
-			
+			int actEnglish = input.nextInt();
+
 			System.out.println("ACT math? ");
-			int actMath = input.nextInt(); //assigning math score
-			
+			int actMath = input.nextInt();
+
 			System.out.println("ACT reading? ");
-			int actReading = input.nextInt(); //assigning reading score
+			int actReading = input.nextInt(); 
 			
 			System.out.println("ACT science? ");
-			int actScience = input.nextInt(); //assigning science score
-			
-			actScore = (double)((2 * actReading + actEnglish + actMath + actScience) / 1.8);
-			//calculating ACT score
+			int actScience = input.nextInt();
+
+			actScore = (double)((2 * actReading + actEnglish + actMath + actScience) / 1.8); // calculating ACT score
 		}
-		
+
 		System.out.println("overall GPA? ");
-		actualGPA = input.nextDouble(); //storing overall GPA
-		
+		actualGPA = input.nextDouble();
+
 		System.out.println("Max GPA? ");
-		maxGPA = input.nextDouble(); //storing max GPA
+		maxGPA = input.nextDouble();
+
+		input.close(); // close Scanner
 		
-		gpaScore = (actualGPA / maxGPA) * 100; //calculating total GPA Score
-		
-		double overallScore2 = 0; //initializing overall score variable for student 2
-		
-		System.out.println(); //empty line to match sample output
-		
-		if (scoreChoice == 1) //checking if SAT score was chosen/calculated
+		gpaScore = (actualGPA / maxGPA) * 100; // calculate total GPA Score
+
+		double overallScore2 = 0; // initialize student 2 overall score
+
+		// calculate student 2 overall score
+		if (scoreChoice == 1)
 			overallScore2 = satScore + gpaScore;
-		
-		else if (scoreChoice == 2)  //checking if ACT score was chosen/calculated
+
+		else if (scoreChoice == 2) 
 			overallScore2 = actScore + gpaScore;
-		
-		System.out.println("First applicant overall score = " + overallScore1
-				+ "\nSecond applicant overall score = " + overallScore2);
-		
-		if (overallScore1 > overallScore2) //checking if 1st applicant has a better score
+
+		System.out.println("\nFirst applicant overall score = " + overallScore1 + 
+				"\nSecond applicant overall score = " + overallScore2);
+
+		// find the applicant with the higher score
+		if (overallScore1 > overallScore2)
 			System.out.println("The first applicant seems to be better");
-		
-		
-		else if (overallScore2 > overallScore1) //checking if 2nd applicant has a better score
+
+		else if (overallScore2 > overallScore1)
 			System.out.println("The second applicant seems to be better");
-		
-		else //checking if the two applicants have equal scores
+
+		else
 			System.out.println("The two applicants seem to be equal");
-		
+
 		/*---------SAMPLE RUN---------
 		 * This program compares two applicants to
 		 * determine which one seems like the stronger
@@ -136,7 +140,7 @@ public class Admissions {
 		 * either SAT or ACT scores plus a weighted GPA.
 		 * 
 		 * Information for the first applicant:
-		 * 	do you have 1) SAT scores or 2) ACT scores? 
+		 * do you have 1) SAT scores or 2) ACT scores? 
 		 * 2
 		 * ACT English? 
 		 * 20
@@ -152,7 +156,7 @@ public class Admissions {
 		 * 4.0
 		 * 
 		 * Information for the second applicant:
-		 * 	do you have 1) SAT scores or 2) ACT scores? 
+		 * do you have 1) SAT scores or 2) ACT scores? 
 		 * 1
 		 * SAT math? 
 		 * 610
